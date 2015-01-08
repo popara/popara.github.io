2b3dc52e67c5050f74d3ee9c09b30f3cb590a0e1
Zeljko Nesic <zeljko.nesic@devana.rs>
2015-01-08 08:49:08 +0100

    compress and deploy

---
+++ js/main.js
@@ -0,0 +1,1 @@
+(function(){}).call(this),function(){require.config({paths:{jquery:"//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min",ligthbox:"js/lightbox"},shim:{lightbox:["jquery"]}}),require(["jquery","lightbox"],function(a,b){})}.call(this)
--- readme.md
+++
@@ -1,1 +0,0 @@
-This is personal page, so people get to know who am I and what I do
